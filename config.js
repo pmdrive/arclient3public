cf6a400f82c008119b931433017920e01ba55acd
pmdrive <72849497+pmdrive@users.noreply.github.com>
2021-10-21 07:36:14 +0000

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': 'kurrxd09',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
 }
