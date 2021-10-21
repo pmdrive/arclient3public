b97f33177bbd5e1766a30a175ed6fb76bafe27ed
pmdrive <72849497+pmdrive@users.noreply.github.com>
2021-10-21 10:53:26 +0000

    Update tekateki.js

--- plugins/tekateki.js
+++ plugins/tekateki.js
@@ -18,10 +18,10 @@ Ketik ${usedPrefix}tete untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tekateki[id] = [
-        await conn.send2Button(m.chat, caption, '© stikerin', 'Bantuan', `.tete`, m),
+        await conn.send2Button(m.chat, caption, '© akbarbotz', 'Bantuan', `.tete`, m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© stikerin', 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
+            if (conn.tekateki[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© akbarbotz', 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
